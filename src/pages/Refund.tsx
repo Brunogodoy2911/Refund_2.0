190d8aa14d32ac68525cae9117dc2b9cdf7fca55
Brunogodoy2911 <brunogodoy29@hotmail.com>
2025-06-14 22:31:17 -0300

    :rocket: Interface concluída

---
+++ src/pages/Refund.tsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { Input } from "../components/Input";
+import { Select } from "../components/Select";
+import fileSvg from "../assets/file.svg";
+import { CATEGORIES, CATEGORIES_KEYS } from "../utils/categories";
+import { Upload } from "../components/Upload";
+import { Button } from "../components/Button";
+import { useNavigate, useParams } from "react-router";
+
+export function Refund() {
+  const [name, setName] = React.useState("Teste");
+  const [amount, setAmount] = React.useState("43");
+  const [category, setCategory] = React.useState("Transport");
+  const [isLoading, setIsLoading] = React.useState(false);
+  const [filename, setFilename] = React.useState<File | null>(null);
+
+  const navigate = useNavigate();
+  const params = useParams<{ id: string }>();
+
+  function onSubmit(e: React.FormEvent) {
+    e.preventDefault();
+
+    if (params.id) {
+      return navigate(-1);
+    }
+
+    navigate("/confirm", { state: { fromSubmit: true } });
+  }
+
+  return (
+    <form
+      onSubmit={onSubmit}
+      className="bg-gray-500 w-full rounded-xl flex flex-col p-10 gap-6 lg:min-w-[512px]"
+    >
+      <header>
+        <h1 className="text-xl font-bold text-gray-100">
+          Solicitação de reembolso
+        </h1>
+
+        <p className="text-sm text-gray-200 mt-2 mb-4">
+          Dados da despesa para solicitar reembolso.
+        </p>
+      </header>
+
+      <Input
+        required
+        legend="Nome da solicitação"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        disabled={!!params.id}
+      />
+
+      <div className="flex gap-3">
+        <Select
+          required
+          legend="Categoria"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          disabled={!!params.id}
+        >
+          {CATEGORIES_KEYS.map((category) => (
+            <option key={category} value={category}>
+              {CATEGORIES[category].name}
+            </option>
+          ))}
+        </Select>
+
+        <Input
+          legend="Valor"
+          required
+          value={amount}
+          onChange={(e) => setAmount(e.target.value)}
+          disabled={!!params.id}
+        />
+      </div>
+
+      {params.id ? (
+        <a
+          href="https://www.rocketseat.com.br/"
+          target="_blank"
+          className="text-sm text-green-100 font-semibold flex items-center justify-center gap-2 my-6 hover: opacity-70 transition ease-linear"
+        >
+          <img src={fileSvg} alt="Ícone do arquivo" />
+          Abrir comprovante
+        </a>
+      ) : (
+        <Upload
+          filename={filename && filename.name}
+          onChange={(e) => e.target.files && setFilename(e.target.files[0])}
+        />
+      )}
+
+      <Button type={"submit"} isLoading={isLoading}>
+        {params.id ? "Voltar" : "Enviar"}
+      </Button>
+    </form>
+  );
+}
